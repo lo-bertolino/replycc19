9c2d53558974371339066d6c2d4744c8c2304284
lo-bertolino <l.bertolino@jetop.com>
2019-03-15 16:40:20 +0100

    Setup

---
+++ main.py
@@ -0,0 +1,8 @@
+in_file = open("1_victoria_lake.txt", "r")
+
+# N:thewidthofthemap
+# M:theheightofthemap
+# C: the number of Customer Headquarters
+# R: representing the maximum number of Reply Offices that can be built
+n, m, c, r = map(int, in_file.readline().split())
+
