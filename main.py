cc8e34799d100ff1c951d7e3c1dafd82cf66fe96
lo-bertolino <l.bertolino@jetop.com>
2019-03-15 17:59:00 +0100

    Lettura file

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import math
 
 class Hq:
     def __init__(self, file):
-        self.x, self.y, self.reward = file.readline().split()
+        self.y, self.x, self.reward = map(int, file.readline().split())
+
+    def __str__(self):
+        return "%s:%s, %s" % (self.x, self.y, self.reward)
 
 
 in_file = open("1_victoria_lake.txt", "r")
@@ -19,6 +22,7 @@ for _ in range(c):
 
 costs = {
     '#': math.inf,  # Non-walkable cell
+    '\n': math.inf,  # a capo
     '~': 800,  # Water
     '*': 200,  # Traffic jam
     '+': 150,  # Dirt
@@ -31,6 +35,24 @@ costs = {
 mappa = []
 for i in range(n):
     row = []
-    for j in range(m):
-        row.append(in_file.read(1))
+    for j in range(m + 1):
+        row.append(costs[in_file.read(1)])
     mappa.append(row)
+
+for i in range(len(hqs) - 1, 0, -1):
+    reachable = False
+    for y in range(hqs[i].y - 1, hqs[i].y + 1, 2):
+        if 0 <= y < n and mappa[y][hqs[i].x] != math.inf:
+            reachable = True
+    for x in range(hqs[i].x - 1, hqs[i].x + 1, 2):
+        if 0 <= x < m and mappa[hqs[i].y][x] != math.inf:
+            reachable = True
+    if not reachable:
+        hqs.remove(hqs[i])
+        continue
+    else:
+        mappa[hqs[i].x][hqs[i].y] = -hqs[i].reward
+print(len(hqs))
+for i in range(n):
+    print(mappa[i])
+
