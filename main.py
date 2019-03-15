c2161e72485a2fc61f56766b4f33c00353f746fa
lo-bertolino <l.bertolino@jetop.com>
2019-03-15 16:48:20 +0100

    Lettura file

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+class Hq:
+    def __init__(self, file):
+        self.x, self.y, self.reward = file.readline().split()
+
+
 in_file = open("1_victoria_lake.txt", "r")
 
-# N:thewidthofthemap
-# M:theheightofthemap
-# C: the number of Customer Headquarters
-# R: representing the maximum number of Reply Offices that can be built
+# N: width of map
+# M: height of map
+# C: number of Headquarters
+# R: maximum number of Offices
 n, m, c, r = map(int, in_file.readline().split())
+hqs = []
+for _ in range(c):
+    hqs.append(Hq(in_file))
 
+mappa = []
+for i in range(n):
+    row = []
+    for j in range(m):
+        row.append(in_file.read(1))
+    mappa.append(row)
